7f7c2f5983a1b96d0b1b1c848e388512c0fd9b0f
ArcosJuan <arcos0juan@gmail.com>
2022-06-15 12:14:21 -0300

    Add Display class. Class from https://github.com/ArcosJuan/ftp-client.git plus manage_exception and success_message methods.

---
+++ src/display.py
@@ -0,0 +1,82 @@
+import sys
+import os
+
+
+class Display:
+	EXIT_METHODS=[]
+
+	@classmethod
+	def add_exit_methods(cls, methods:list): cls.EXIT_METHODS.extend(methods)
+    
+
+	@classmethod
+	def exit(cls):
+		[method() for method in cls.EXIT_METHODS]
+    
+    
+	@classmethod
+	def ask_question(cls, question: str=''):
+		""" Receives a question, asks it to the user and returns his answer.
+			Also catches the user's exit attempts 
+			and redirects them to the save method.
+		"""
+
+		if question: print(question)
+		answ = input()
+		print('')
+
+		if answ == 'quit' or answ == 'exit': cls.exit()
+		else: return answ
+
+
+	@classmethod
+	def execute_operation(cls, msg, method, *args):
+		print(f"{msg}...\r", end='') 
+		result = method(*args)
+		print("Operation completed!")
+		cls.clear()
+		return result
+
+
+	@staticmethod
+	def clear(): os.system('cls' if os.name=='nt' else 'clear')
+
+
+	@staticmethod
+	def list_to_str(list):
+		""" Receives a list and returns a human-readable string of elements.
+		"""
+		string = ''
+		for element in list:
+			string += str(element) + ' \n'
+		return string
+
+
+	@staticmethod
+	def enum_to_list(enum):
+		""" Returns the list of all the elements 
+			of a given enum and its values.
+		"""
+        
+		return [f'{str(element.value)}: {str(element)}' for element in enum]
+        
+        
+	@staticmethod
+	def manage_exception(response, exception="", quit=False):
+		Display.clear()
+		
+		print("Response:")
+		
+		print(
+			'\033[91m' 
+			+ response 
+			+ ('\n' if exception else '')
+			+ f"{exception}" 
+			+ '\033[0m', 
+			end='\n\n'
+			)
+		if (quit): Display.exit()
+
+	
+	@staticmethod
+	def success_message(msg): print('\033[92m' + msg + '\033[0m', end='\n\n')
